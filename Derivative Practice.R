92ba82e129c4db4b7cc2299d5f075af9f8dec5d6
Awoo56709 <62405610+Awoo56709@users.noreply.github.com>
2025-08-05 14:33:43 -0700

    Starting Parital Practice

--- Derivative Practice.R
+++ Derivative Practice.R
@@ -60,14 +60,14 @@ eval(dt_dy)
 library(ggplot2)
 library(palmerpenguins)
 
-ggplot(data = penguins, aes(x = body_mass_g, y = flipper_length_mm)) + geom_point(aes(color = species)) + scale_color_manual(values = c("darkorange", "purple", "cyan4")) 
-# +
- # labs(x = "Body mass (g)",
-   #    y = "Flipper length (mm)",
-  #     title = "Palmer penguin size measurements"
-    #   subtitle = "Palmer archipelago, Antartica (2007 - 2009)",
-    #   caption = "Collected by Dr. Kristen Gorman") +
- # facet_wrap(~island)
-#theme_minimal()
+ggplot(data = penguins, aes(x = body_mass_g, y = flipper_length_mm)) + geom_point(aes(color = species)) + scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+
+ labs(x = "Body mass (g)",
+      y = "Flipper length (mm)",
+      title = "Palmer penguin size measurements",
+       subtitle = "Palmer archipelago, Antartica (2007 - 2009)",
+      caption = "Collected by Dr. Kristen Gorman") +
+ facet_wrap(~island)
+theme_minimal()
                                                                                                                                                                             
                                                                                   
